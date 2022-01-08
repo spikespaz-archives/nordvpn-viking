4cccf071f613183442f662a283fe678504ba40ad
Jacob Birkett <jacob@birkett.dev>
2022-01-08 10:09:04 -0700

    Change build.rs to use git instead of zip download

--- build.rs
+++ build.rs
@@ -1,52 +1,57 @@
-use reqwest::blocking as reqwest;
+use git2::{build::CheckoutBuilder, Oid, Repository};
+use slug::slugify;
 use std::{
     env, fs,
-    fs::File,
-    io,
-    io::Cursor,
     path::{Path, PathBuf},
 };
-use zip::read::ZipArchive;
+use toml::Value;
 
-fn download_flag_icons(url: &str, dest_dir: &str) {
-    let response = reqwest::get(url).unwrap();
-    let content = response.bytes().unwrap();
-    let mut zip = ZipArchive::new(Cursor::new(content)).unwrap();
-    let dest_dir = Path::new(dest_dir);
-
-    for index in 0..zip.len() {
-        let mut zip_file = zip.by_index(index).unwrap();
-        let zip_file_path = match zip_file.enclosed_name() {
-            Some(path) => path,
-            None => continue,
-        };
-
-        const FLAGS_PATH_PREFIX: &str = "flag-icons-main/flags";
-
-        if !zip_file.is_file()
-            || !zip_file_path.starts_with(FLAGS_PATH_PREFIX)
-            || zip_file_path.extension().unwrap() != "svg"
-        {
-            continue;
+fn update_repository<P: AsRef<Path>>(url: &str, commit: &str, dest_dir: P) -> bool {
+    let mut updated = false;
+    let repository = match Repository::open(&dest_dir) {
+        Ok(repository) => repository,
+        Err(_) => {
+            updated = true;
+            Repository::clone(url, &dest_dir).unwrap()
         }
+    };
+    let commit_old = repository.head().unwrap().target().unwrap();
+    let commit_new = Oid::from_str(commit).unwrap();
 
-        let dest_file_path = dest_dir.join(zip_file_path.strip_prefix(FLAGS_PATH_PREFIX).unwrap());
-        fs::create_dir_all(dest_file_path.parent().unwrap()).unwrap();
-        let mut dest_file = File::create(&dest_file_path).unwrap();
-        io::copy(&mut zip_file, &mut dest_file).unwrap();
+    if commit_old != commit_new {
+        updated = true;
+        repository.set_head_detached(commit_new).unwrap();
+        repository
+            .checkout_head(Some(&mut CheckoutBuilder::default().force()))
+            .unwrap();
     }
+
+    updated
 }
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    println!("Output directory: {:?}", out_dir);
+
+    let manifest = fs::read_to_string("Cargo.toml")
+        .unwrap()
+        .parse::<Value>()
+        .unwrap();
+    let manifest = manifest.as_table().unwrap();
+    let metadata = manifest["package"]["metadata"].as_table().unwrap();
+
+    let flag_icons_metadata = metadata["flag-icons"].as_table().unwrap();
+    let flag_icons_git = flag_icons_metadata["git"].as_str().unwrap();
+    let flag_icons_commit = flag_icons_metadata["commit"].as_str().unwrap();
+    let flag_icons_clone_dest = out_dir.join(slugify(flag_icons_git));
+
+    let result = update_repository(flag_icons_git, flag_icons_commit, flag_icons_clone_dest);
+
+    println!("Repository updated: {}", result);
+
+    panic!();
+
     // let out_res_dir = out_dir.join("res");
     // let out_res_icons_dir = out_dir.join("res/icons");
-    let out_res_icons_flags_dir = out_dir.join("res/icons/flags");
-
-    let flag_icons_zip_url = env::var("FLAG_ICONS_ZIP_URL").unwrap();
-
-    download_flag_icons(
-        &flag_icons_zip_url,
-        out_res_icons_flags_dir.to_str().unwrap(),
-    );
 }
