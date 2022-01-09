3114bf3f16277870303185f65c0ebb07d2bf1779
Jacob Birkett <jacob@birkett.dev>
2022-01-08 19:48:30 -0700

    Use cargo_toml for my convenience

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
+use cargo_toml::Manifest;
 use git2::{build::CheckoutBuilder, Oid, Repository};
+use serde::Deserialize;
 use slug::slugify;
 use std::{
-    env, fs,
+    collections::BTreeMap,
+    env,
     path::{Path, PathBuf},
 };
-use toml::Value;
 
 fn update_repository<P: AsRef<Path>>(url: &str, commit: &str, dest_dir: P) -> bool {
     let mut updated = false;
@@ -29,26 +31,36 @@ fn update_repository<P: AsRef<Path>>(url: &str, commit: &str, dest_dir: P) -> bo
     updated
 }
 
+type ForeignDepsSet = BTreeMap<String, ForeignDependency>;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct Metadata {
+    foreign_dependencies: ForeignDepsSet,
+}
+
+#[derive(Debug, Deserialize)]
+struct ForeignDependency {
+    git: String,
+    commit: String,
+    copy: Vec<(String, String)>,
+}
+
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let manifest: Manifest<Metadata> = Manifest::from_path_with_metadata("Cargo.toml").unwrap();
 
     println!("Output directory: {:?}", out_dir);
+    println!("Manifest: {:#?}", manifest);
 
-    let manifest = fs::read_to_string("Cargo.toml")
-        .unwrap()
-        .parse::<Value>()
-        .unwrap();
-    let manifest = manifest.as_table().unwrap();
-    let metadata = manifest["package"]["metadata"].as_table().unwrap();
+    // let flag_icons_metadata = metadata["flag-icons"].as_table().unwrap();
+    // let flag_icons_git = flag_icons_metadata["git"].as_str().unwrap();
+    // let flag_icons_commit = flag_icons_metadata["commit"].as_str().unwrap();
+    // let flag_icons_clone_dest = out_dir.join(slugify(flag_icons_git));
 
-    let flag_icons_metadata = metadata["flag-icons"].as_table().unwrap();
-    let flag_icons_git = flag_icons_metadata["git"].as_str().unwrap();
-    let flag_icons_commit = flag_icons_metadata["commit"].as_str().unwrap();
-    let flag_icons_clone_dest = out_dir.join(slugify(flag_icons_git));
+    // let result = update_repository(flag_icons_git, flag_icons_commit, flag_icons_clone_dest);
 
-    let result = update_repository(flag_icons_git, flag_icons_commit, flag_icons_clone_dest);
-
-    println!("Repository updated: {}", result);
+    // println!("Repository updated: {}", result);
 
     panic!();
 
