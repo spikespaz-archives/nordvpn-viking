c47823c86df4bed418121cd756511031e7a9776a
Jacob Birkett <jacob@birkett.dev>
2022-01-09 17:05:38 -0700

    Change tests to use test_case crate

--- build-helpers/src/gresource.rs
+++ build-helpers/src/gresource.rs
@@ -26,103 +26,62 @@ pub enum Preprocess {
 mod tests {
     use crate::gresource::*;
     use serde_xml_rs::{from_str, to_string, Deserializer, Serializer};
+    use test_case::test_case;
     use xml::reader::{EventReader, ParserConfig};
 
-    #[test]
-    fn test_deserialize_file() {
-        {
-            let xml = r#"<file>foo/bar/icon.png</file>"#;
-            let expected = File {
+    #[test_case(
+        r#"<file>foo/bar/icon.png</file>"#,
+        File {
                 path: "foo/bar/icon.png".to_owned(),
                 alias: None,
                 compressed: false,
                 preprocess: None,
-            };
-
-            let config = ParserConfig::new()
-                .trim_whitespace(false)
-                .whitespace_to_characters(true);
-            let event_reader = EventReader::new_with_config(xml.as_bytes(), config);
-            let item = File::deserialize(&mut Deserializer::new(event_reader)).unwrap();
-
-            println!("{:#?}", item);
-            assert_eq!(item, expected);
         }
-
-        {
-            let xml = r#"<file alias="image.png">foo/bar/icon.png</file>"#;
-            let expected = File {
+    )]
+    #[test_case(
+        r#"<file alias="icon.png">foo/bar/icon.png</file>"#,
+        File {
                 path: "foo/bar/icon.png".to_owned(),
-                alias: Some("image.png".to_owned()),
+            alias: Some("icon.png".to_owned()),
                 compressed: false,
                 preprocess: None,
-            };
-
-            let config = ParserConfig::new()
-                .trim_whitespace(false)
-                .whitespace_to_characters(true);
-            let event_reader = EventReader::new_with_config(xml.as_bytes(), config);
-            let item = File::deserialize(&mut Deserializer::new(event_reader)).unwrap();
-
-            println!("{:#?}", item);
-            assert_eq!(item, expected);
         }
-
-        {
-            let xml = r#"<file compressed="true">foo/bar/icon.png</file>"#;
-            let expected = File {
+    )]
+    #[test_case(
+        r#"<file preprocess="to-pixdata">foo/bar/icon.png</file>"#,
+        File {
                 path: "foo/bar/icon.png".to_owned(),
                 alias: None,
-                compressed: true,
-                preprocess: None,
-            };
-
-            let config = ParserConfig::new()
-                .trim_whitespace(false)
-                .whitespace_to_characters(true);
-            let event_reader = EventReader::new_with_config(xml.as_bytes(), config);
-            let item = File::deserialize(&mut Deserializer::new(event_reader)).unwrap();
-
-            println!("{:#?}", item);
-            assert_eq!(item, expected);
+            compressed: false,
+            preprocess: Some(Preprocess::ToPixData),
         }
-
-        {
-            let xml = r#"<file preprocess="to-pixdata">foo/bar/icon.png</file>"#;
-            let expected = File {
+    )]
+    #[test_case(
+        r#"<file preprocess="xml-stripblanks">foo/bar/vector.svg</file>"#,
+        File {
+            path: "foo/bar/vector.svg".to_owned(),
+            alias: None,
+            compressed: false,
+            preprocess: Some(Preprocess::XmlStripBlanks),
+        }
+    )]
+    #[test_case(
+        r#"<file compressed="true" preprocess="to-pixdata">foo/bar/icon.png</file>"#,
+        File {
                 path: "foo/bar/icon.png".to_owned(),
                 alias: None,
-                compressed: false,
+            compressed: true,
                 preprocess: Some(Preprocess::ToPixData),
-            };
-
+        }
+    )]
+    fn test_deserialize_file(xml: &str, expected: File) {
             let config = ParserConfig::new()
                 .trim_whitespace(false)
                 .whitespace_to_characters(true);
+
             let event_reader = EventReader::new_with_config(xml.as_bytes(), config);
             let item = File::deserialize(&mut Deserializer::new(event_reader)).unwrap();
 
-            println!("{:#?}", item);
             assert_eq!(item, expected);
-        }
-
-        {
-            let xml = r#"<file alias="vector.svg" compressed="true" preprocess="xml-stripblanks">foo/bar/icon.svg</file>"#;
-            let expected = File {
-                path: "foo/bar/icon.svg".to_owned(),
-                alias: Some("vector.svg".to_owned()),
-                compressed: true,
-                preprocess: Some(Preprocess::XmlStripBlanks),
-            };
-
-            let config = ParserConfig::new()
-                .trim_whitespace(false)
-                .whitespace_to_characters(true);
-            let event_reader = EventReader::new_with_config(xml.as_bytes(), config);
-            let item = File::deserialize(&mut Deserializer::new(event_reader)).unwrap();
-
-            println!("{:#?}", item);
-            assert_eq!(item, expected);
-        }
     }
 }
