de66b8efcedaf624a1065720f6a38daaf7176202
Jacob Birkett <jacob@birkett.dev>
2022-01-11 20:08:42 -0700

    Assign metadata from manifest for ease

--- build.rs
+++ build.rs
@@ -14,18 +14,12 @@ struct Metadata {
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let manifest: Manifest<Metadata> = Manifest::from_path_with_metadata("Cargo.toml").unwrap();
+    let metadata = manifest.package.unwrap().metadata.unwrap();
 
     println!("Output directory: {:?}", out_dir);
     // println!("Manifest: {:#?}", manifest);
 
-    for (name, detail) in manifest
-        .package
-        .unwrap()
-        .metadata
-        .unwrap()
-        .foreign_dependencies
-        .into_iter()
-    {
+    for (name, detail) in metadata.foreign_dependencies.into_iter() {
         let updated = detail.update(&out_dir);
         if !updated {
             continue;
