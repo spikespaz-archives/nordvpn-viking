49be41bc4fefd3d59f5b88b180f8bfc9c14f9b03
Jacob Birkett <jacob@birkett.dev>
2022-01-11 21:15:05 -0700

    Add iterables for expanding the details to xml descripts

--- build-helpers/src/manifest.rs
+++ build-helpers/src/manifest.rs
@@ -1,7 +1,9 @@
 use crate::foreign_dependencies::ForeignDependency;
-use crate::gresources::Preprocess;
+use crate::gresources::{File, GResource, Preprocess};
+use glob::glob;
 use serde::Deserialize;
 use std::collections::BTreeMap;
+use std::path::Path;
 
 pub type ForeignDepsSet = BTreeMap<String, ForeignDependency>;
 pub type GResourceSet = BTreeMap<String, GResourceDetail>;
@@ -25,3 +27,48 @@ pub struct GResourceDetail {
     pub files: Vec<GResourceFilesDetail>,
 }
 
+impl GResourceFilesDetail {
+    pub fn to_file_iter<P: AsRef<Path>>(&self, out_dir: P) -> GResourceFilesDetailIter {
+        GResourceFilesDetailIter::new(&self, out_dir)
+    }
+}
+
+impl<'a> GResourceFilesDetailIter<'a> {
+    pub fn new<P: AsRef<Path>>(inner: &'a GResourceFilesDetail, out_dir: P) -> Self {
+        Self {
+            inner,
+            glob: glob(out_dir.as_ref().to_str().unwrap()).unwrap(),
+        }
+    }
+}
+
+impl Iterator for GResourceFilesDetailIter<'_> {
+    type Item = File;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let file_path = self.glob.next()?.unwrap();
+        let file_name = file_path.file_name().unwrap().to_str().unwrap();
+        let alias = self
+            .inner
+            .alias
+            .clone()
+            .map(|alias| alias.replace("{}", file_name));
+
+        Some(File::new(
+            file_path.to_str().to_owned().unwrap().to_string(),
+            alias.map(|alias| alias.clone()),
+            self.inner.compressed.clone(),
+            self.inner.preprocess.clone(),
+        ))
+    }
+}
+
+impl GResourceDetail {
+    pub fn to_gresource<P: AsRef<Path>>(&self, out_dir: P) -> GResource {
+        let files = self
+            .files
+            .iter()
+            .flat_map(|detail| detail.to_file_iter(&out_dir));
+        GResource::from_iter(self.prefix.clone(), files)
+    }
+}
